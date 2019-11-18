de87a8547a6ec85dc484236c781fb2410d53a1cc
jamo1909 <rjmjmurphy@gmail.com>
2019-11-18 22:53:46 +0000

    Aircraft return IATA code

--- routes/search.js
+++ routes/search.js
@@ -1,11 +1,85 @@
 var express = require('express');
 var router = express.Router();
+const {Client} = require('pg');
+var plane = require('../public/javascripts/getSinglePlane'); //Calls plane icao
+var modelDb = require('../public/javascripts/db/aircraftModelQuery');
 
+
+const model = new Client({
+    user: "postgres",
+    password: "jamo1818",
+    host: "localhost",
+    port: 5433,
+    database: "aircraftModel"
+});
+const code = new Client({
+    user: "postgres",
+    password: "jamo1818",
+    host: "localhost",
+    port: 5433,
+    database: "aircraftModel"
+});
+
+let thisPlane = {
+    icao: "",
+    iata: "",
+    manufacture: "",
+    model: "",
+    owner: "",
+    modelIcao: ""
+};
+
+
+plane().then(result => {
+    console.log("Returning plane");
+    thisPlane.icao = result.icao;
+    aircraftDatabase(thisPlane.icao);
+    codeConvertion('A320');
+}).catch(err => {
+    console.log(err);
+});
+
+
+//Collect origin airport long/lat
+function setAircraftInfo(aircraftData) {
+    console.log("Name: " + aircraftData.rows[0].icao24);
+    thisPlane.manufacture = aircraftData.rows[0].manufacture;
+    thisPlane.model = aircraftData.rows[0].model;
+    thisPlane.owner = aircraftData.rows[0].owner;
+    thisPlane.modelIcao = aircraftData.rows[0].typecode;
+}
+
+function aircraftIata(aircraftData) {
+    console.log("Name: " + aircraftData.rows[0].iata);
+    thisPlane.iata = aircraftData.rows[0].iata;
+    console.log(thisPlane);
+}
+
+function aircraftDatabase(airportCode) {
+    model.connect()
+        .then(() => console.log("Connected successfuly"))
+        .then(() => model.query("SELECT * from Public.\"aircraftInformation\" where icao24 = $1", [airportCode]))
+        .then(results => setAircraftInfo(results))
+        .catch(e => console.log(e))
+        .finally(() => model.end())
+}
+
+function codeConvertion(airportCode) {
+    code.connect()
+        .then(() => console.log("Connected successfuly"))
+        .then(() => code.query("SELECT distinct *  FROM Public.\"aircraftCodeConvertion\" where icao=$1", [airportCode]))
+        .then(results => aircraftIata(results))
+        .catch(e => console.log(e))
+        .finally(() => code.end())
+}
+
+function shorten(str) {
+    thisPlane = thisPlane.model.length - 1;
+}
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('search', {
-        title: 'OpenSky Network Real Time Emissions',
-        name: 'Search planes'
+        test: test
     });
 
 });
