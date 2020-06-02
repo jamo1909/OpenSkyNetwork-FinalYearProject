5a2bae2342621ef31e71ee5f8d8a7febf0fa08de
jamo1909 <rjmjmurphy@gmail.com>
2020-06-02 01:54:02 +0100

    Email Setup to Jmurphy1909@gmail.com

--- routes/messageSent.js
+++ routes/messageSent.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET map page. */
 router.post('/', function (req, res) {
-    var name = req.body.name
-    res.render('messageSent', {
-        name: name,
+    var name = req.body.name;
+    var email = req.body.email;
+    var subject = req.body.subject;
+    var message = req.body.message;
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'jmurphy1909@gmail.com',
+            pass: 'Courtown1818'
+        }
     });
+
+    var mailOptions = {
+        from:'jmurphy1909@gmail.com',
+        to: email,
+        subject: subject,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.render('messageSent', {
+                name: name,
+            });
+        }
+    });
+
+
+
+
 
 });
 
